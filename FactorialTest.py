b2ae5e73457de49958aeb68ba26ce8f637e2f3f5
Massimo Nocentini <massimo.nocentini@gmail.com>
2016-10-05 09:08:43 +0200

    Starting point of TDD about the `factorial` example seen during class.

---
+++ FactorialTest.py
@@ -0,0 +1,25 @@
+
+import unittest
+from factorial import Factorial
+
+class FactorialTest(unittest.TestCase):
+
+#A testcase is created by subclassing unittest.TestCase. The three individual tests are defined with methods whose names start with the letters test. This naming convention informs the test runner about which methods represent tests.
+
+#The crux of each test is a call to assertEqual() to check for an expected result; assertTrue() or assertFalse() to verify a condition; or assertRaises() to verify that a specific exception gets raised. These methods are used instead of the assert statement so the test runner can accumulate all test results and produce a report.
+
+    def setUp(self):
+        self.factorial = Factorial()
+
+    def test_base_case(self):
+        self.assertEqual(1, self.factorial(0))
+
+    def test_non_base_case(self):
+        self.assertEqual(120, self.factorial(5))
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ factorial.py
@@ -0,0 +1,5 @@
+
+class Factorial(object):
+
+    def __call__(self, i):
+        return 120 if i else 1
