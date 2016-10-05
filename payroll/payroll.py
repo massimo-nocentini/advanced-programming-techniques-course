a5caddb784cfc18d049259b6b686201fb85acbec
Massimo Nocentini <massimo.nocentini@gmail.com>
2016-10-05 14:11:22 +0200

    First setup of payroll example to show mocking methodology.

---
+++ payroll/payroll.py
@@ -0,0 +1,3 @@
+
+class PayRoll:
+    pass
---
+++ payroll/payroll_test.py
@@ -0,0 +1,9 @@
+
+import unittest
+
+from payroll import PayRoll
+
+class PayrollTest(unittest.TestCase):
+
+    def setUp(self):
+        self.payRoll = PayRoll()
