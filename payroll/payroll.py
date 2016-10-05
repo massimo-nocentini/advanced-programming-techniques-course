b7e8fbec707cc6f4cc97af73368fdaf5864fe280
Massimo Nocentini <massimo.nocentini@gmail.com>
2016-10-05 15:23:17 +0200

    Refactor upto paying a single employee.

--- payroll/payroll.py
+++ payroll/payroll.py
@@ -1,12 +1,22 @@
 
 class PayRoll:
 
-    def __init__(self, employeeDB):
+    def __init__(self, employeeDB, bank_service):
         self.employeeDB = employeeDB
+        self.bank_service = bank_service
 
     def monthly_payment(self):
-        return len(self.employeeDB.getAllEmployees())
+        employees = self.employeeDB.getAllEmployees()
+        if employees:
+            e, *rest = employees
+            e.paid_by(self.bank_service)
+        return len(employees)
 
-class Employee(): 
+class Employee: 
     
-    pass
+    def __init__(self, identifier, salary):
+        self.identifier = identifier
+        self.salary = salary
+
+    def paid_by(self, bank_service):
+        bank_service.make_payment(employee_id=self.identifier, salary=self.salary)
--- payroll/payroll_test.py
+++ payroll/payroll_test.py
@@ -10,10 +10,11 @@ class PayrollTest(unittest.TestCase):
 
         self.employees = []
         self.employeeDB = Mock()
+        self.bank_service = Mock()
 
         self.employeeDB.getAllEmployees.return_value=self.employees # stubbing
 
-        self.payRoll = PayRoll(self.employeeDB)
+        self.payRoll = PayRoll(self.employeeDB, self.bank_service)
 
     def test_no_employees(self):
         self.assertEqual(0, self.payRoll.monthly_payment())
@@ -22,6 +23,19 @@ class PayrollTest(unittest.TestCase):
         self.employees.append(self._make_employee())
         self.assertEqual(1, self.payRoll.monthly_payment())
 
+    def test_interaction_with_db_happened(self):
+        _ = self.payRoll.monthly_payment()
+        self.assertTrue(self.employeeDB.getAllEmployees.called)
 
-    def _make_employee(self):
-        return Employee()
+    def test_only_one_interaction_with_db(self):
+        _ = self.payRoll.monthly_payment()
+        self.assertEqual(1, self.employeeDB.getAllEmployees.call_count)
+
+    def test_employee_is_paid(self):
+        self.employees.append(self._make_employee(identifier='id1', salary=1000))
+        self.assertEqual(1, self.payRoll.monthly_payment())
+        self.bank_service.make_payment.assert_called_once_with(employee_id='id1', salary=1000)
+
+
+    def _make_employee(self, identifier=None, salary=None):
+        return Employee(identifier, salary)
