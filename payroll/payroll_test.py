93485ae2f335f466c3138c444e82a199b11e3287
Massimo Nocentini <massimo.nocentini@gmail.com>
2016-10-05 14:46:10 +0200

    Refactoring of stubbing moving it in `setUp` method.

--- payroll/payroll_test.py
+++ payroll/payroll_test.py
@@ -7,14 +7,21 @@ from payroll import PayRoll, Employee
 class PayrollTest(unittest.TestCase):
 
     def setUp(self):
+
+        self.employees = []
         self.employeeDB = Mock()
+
+        self.employeeDB.getAllEmployees.return_value=self.employees # stubbing
+
         self.payRoll = PayRoll(self.employeeDB)
 
     def test_no_employees(self):
-        self.employeeDB.getAllEmployees.return_value=[] # stubbing
         self.assertEqual(0, self.payRoll.monthly_payment())
 
     def test_single_employee(self):
-        employees = [Employee()]
-        self.employeeDB.getAllEmployees.return_value=employees # stubbing
+        self.employees.append(self._make_employee())
         self.assertEqual(1, self.payRoll.monthly_payment())
+
+
+    def _make_employee(self):
+        return Employee()
