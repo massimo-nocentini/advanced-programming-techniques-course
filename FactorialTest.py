70b661b7aa2dbf229bc2da729ef71919db64bb2a
Massimo Nocentini <massimo.nocentini@gmail.com>
2016-10-05 09:21:31 +0200

    Naive `RecursionError` excepting.

--- FactorialTest.py
+++ FactorialTest.py
@@ -4,10 +4,6 @@ from factorial import Factorial
 
 class FactorialTest(unittest.TestCase):
 
-#A testcase is created by subclassing unittest.TestCase. The three individual tests are defined with methods whose names start with the letters test. This naming convention informs the test runner about which methods represent tests.
-
-#The crux of each test is a call to assertEqual() to check for an expected result; assertTrue() or assertFalse() to verify a condition; or assertRaises() to verify that a specific exception gets raised. These methods are used instead of the assert statement so the test runner can accumulate all test results and produce a report.
-
     def setUp(self):
         self.factorial = Factorial()
 
@@ -17,3 +13,8 @@ class FactorialTest(unittest.TestCase):
     def test_non_base_case(self, i=5):
         self.assertEqual(self.factorial(i), i * self.factorial(i-1))
 
+    def test_negative_input(self):
+        try:
+            self.factorial(-1)
+            raise Exception("should not be here")
+        except RecursionError: pass
