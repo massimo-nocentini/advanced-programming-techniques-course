7ea2e4b030061692643d3c0bffddfb91483b2e6c
Massimo Nocentini <massimo.nocentini@gmail.com>
2016-10-22 21:33:59 +0200

    parameterize method selection using a lambda expr

--- tdd/tdd.py
+++ tdd/tdd.py
@@ -1,14 +1,16 @@
 
 class WasRun:
 
-    def __init__(self, name):
+    # Now class `WasRun` is doing two distinct jobs: one is keeping track whether
+    # a method was invoked or not, and the other is (dynamically) invoking the method.
+
+    def __init__(self, getter):
         self.was_run = False
+        self.getter = getter
 
     def test_method(self):
         self.was_run = True 
     
     def run(self):
-        # lots of refactorings has this feel -- separating two parts so you
-        # work on them separately. If they go back together when you are finished, fine;
-        # if not, you can leave them separate.
-        self.test_method()
+        method = self.getter(self)
+        method()
--- tdd/tdd_test.py
+++ tdd/tdd_test.py
@@ -1,11 +1,10 @@
 
 from tdd import *
 
-test = WasRun("testMethod")
+test = WasRun(lambda sut: sut.test_method)
+
 print(test.was_run) # we expect to see `False` printed out
 
-# next we need to use our real interface `run()`, instead
-# of calling the test method directly:
 test.run()
 
 print(test.was_run) # we expect to see `True` printed out
