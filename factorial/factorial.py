8b715c4dea6f30559647b2c18db98927ab991d82
Massimo Nocentini <massimo.nocentini@gmail.com>
2017-01-24 13:56:46 +0100

    Make `factorial` iterative.

--- factorial/factorial.py
+++ factorial/factorial.py
@@ -3,4 +3,10 @@ class Factorial(object):
 
     def __call__(self, i):
         if i < 0: raise ValueError
-        return i*self(i-1) if i else 1
+
+        result = 1
+        while i:
+            result *= i
+            i -= 1
+
+        return result
